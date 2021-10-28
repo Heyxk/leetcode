def894d6bd6230a847031cd6977a9eb47f6eb9af
Heyxk <1521048028@qq.com>
2021-10-28 05:46:01 +0000

    剑指 Offer 04. 二维数组中的查找

---
+++ codingInterviews/04/main_test.go
@@ -0,0 +1,33 @@
+package main
+
+import "testing"
+
+func Test_findNumberIn2DArray(t *testing.T) {
+	type args struct {
+		matrix [][]int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{"caes1", args{[][]int{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}}, 5}, true},
+		{"caes2", args{[][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}, 10}, false},
+		{"caes3", args{[][]int{{10}}, 10}, true},
+		{"caes4", args{[][]int{}, 10}, false},
+		{"caes5", args{[][]int{{-1, 3}}, 3}, true},
+		{"caes6", args{[][]int{{-1, 3}, {2, 4}, {6, 9}}, 6}, true},
+		{"caes7", args{[][]int{{-1}, {2}}, 2}, true},
+		{"caes8", args{[][]int{{1, 3, 5}}, 1}, true},
+		{"caes9", args{[][]int{{}}, 1}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findNumberIn2DArray(tt.args.matrix, tt.args.target); got != tt.want {
+				t.Errorf("searchMatrix() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
