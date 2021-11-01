3702ec6369a4f53726ada6d3ff94472612906b12
Heyxk <1521048028@qq.com>
2021-11-01 09:30:37 +0000

    剑指 Offer 10- I. 斐波那契数列

---
+++ codingInterviews/10/main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import "testing"
+
+func Test_fib(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{"case1", args{2}, 1},
+		{"case2", args{5}, 5},
+		{"case3", args{0}, 0},
+		{"case4", args{1}, 1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := fib(tt.args.n); got != tt.want {
+				t.Errorf("fib() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
