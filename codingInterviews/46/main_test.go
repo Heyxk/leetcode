a5fc13a226e907cacabd5ebda07783b02e4c281a
Heyxk <1521048028@qq.com>
2021-11-02 09:33:38 +0000

    剑指 Offer 46. 把数字翻译成字符串

---
+++ codingInterviews/46/main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func Test_translateNum(t *testing.T) {
+	type args struct {
+		num int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{"case1", args{12258}, 5},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := translateNum(tt.args.num); got != tt.want {
+				t.Errorf("translateNum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
