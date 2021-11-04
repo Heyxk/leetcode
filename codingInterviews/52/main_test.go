ed90847b0571b78161e9ce694e1b7a11e2c3897d
Heyxk <1521048028@qq.com>
2021-11-04 09:50:18 +0000

    剑指 Offer 52. 两个链表的第一个公共节点

---
+++ codingInterviews/52/main_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_getIntersectionNode(t *testing.T) {
+	type args struct {
+		headA *ListNode
+		headB *ListNode
+	}
+
+	a := &ListNode{4, nil}
+	b := &ListNode{1, nil}
+	c := &ListNode{8, nil}
+	d := &ListNode{4, nil}
+	e := &ListNode{5, nil}
+
+	a.Next = b
+	b.Next = c
+	c.Next = d
+	d.Next = e
+
+	aa := &ListNode{5, nil}
+	bb := &ListNode{0, nil}
+	cc := &ListNode{1, nil}
+	aa.Next = bb
+	bb.Next = cc
+	cc.Next = c
+
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		// TODO: Add test cases.
+		{"case1", args{a, aa}, c},
+		{"case2", args{nil, nil}, nil},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getIntersectionNode(tt.args.headA, tt.args.headB); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getIntersectionNode() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
