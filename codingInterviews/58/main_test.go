7b476042d051c027d102e7c20ed90d39fad0f358
Heyxk <1521048028@qq.com>
2022-07-27 21:51:27 +0800

    剑指 Offer 58 - II. 左旋转字符串

--- codingInterviews/58/main_test.go
+++ codingInterviews/58/main_test.go
@@ -17,6 +17,7 @@ func Test_reverseLeftWords(t *testing.T) {
 		{"case2", args{"abcdefg", 0}, "abcdefg"},
 		{"case3", args{"", 2}, ""},
 		{"case1", args{"a", 1}, "a"},
+		{"case1", args{"lrloseumgh", 6}, "umghlrlose"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
