e0d9db8d53893b22a0efd814eb4932ba5633bc8c
Heyxk <1521048028@qq.com>
2022-07-27 05:00:54 +0000

    剑指 Offer 06. 从尾到头打印链表

--- codingInterviews/06/main.go
+++ codingInterviews/06/main.go
@@ -7,20 +7,17 @@ package main
  *     Next *ListNode
  * }
  */
-func reversePrint(head *ListNode) []int {
-	if nil == head {
-		return []int{}
-	}
-	var ret []int
-	// tmp := make([]int, 1)
-	for ; head != nil; head = head.Next {
-		tmp := []int{head.Val}
-		ret = append(tmp, ret...)
-	}
-	return ret
-}
 
 type ListNode struct {
 	Val  int
 	Next *ListNode
+}
+
+func reversePrint(head *ListNode) []int {
+	ret := []int{}
+	for head != nil {
+		ret = append([]int{head.Val}, ret...)
+		head = head.Next
+	}
+	return ret
 }
--- codingInterviews/06/main_test.go
+++ codingInterviews/06/main_test.go
@@ -17,6 +17,8 @@ func Test_reversePrint(t *testing.T) {
 		// TODO: Add test cases.
 		{"case1", args{&ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: nil}}}}, []int{3, 2, 1}},
 		{"case2", args{nil}, []int{}},
+		{"case3", args{&ListNode{Val: 2, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: nil}}}}, []int{3, 2, 2}},
+		{"case4", args{&ListNode{Val: 1, Next: nil}}, []int{1}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -24,5 +26,27 @@ func Test_reversePrint(t *testing.T) {
 				t.Errorf("reversePrint() = %v, want %v", got, tt.want)
 			}
 		})
+	}
+}
+
+func BenchmarkLeetCode(b *testing.B) {
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+		{"case1", args{&ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: nil}}}}, []int{3, 2, 1}},
+		{"case2", args{nil}, []int{}},
+		{"case3", args{&ListNode{Val: 2, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: nil}}}}, []int{3, 2, 2}},
+		{"case4", args{&ListNode{Val: 1, Next: nil}}, []int{1}},
+	}
+	for i := 0; i < b.N; i++ {
+		for _, tt := range tests {
+			reversePrint(tt.args.head)
+		}
 	}
 }
