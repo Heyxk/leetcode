d5e324de57fd08381c89ddcc926cef9055e5963c
Heyxk <1521048028@qq.com>
2022-07-28 04:42:42 +0000

    剑指 Offer 53 - I. 在排序数组中查找数字 I

--- codingInterviews/53/main.go
+++ codingInterviews/53/main.go
@@ -1,32 +1,13 @@
 package main
 
 func search(nums []int, target int) int {
-	length := len(nums)
-	left := 0
-	right := length
-	ret := 0
-LABLE:
-	for cus := (left + right) / 2; left < right; cus = (left + right) / 2 {
-		switch {
-		case nums[cus] > target:
-			if right == cus {
-				break LABLE
-			}
-			right = cus
-		case nums[cus] < target:
-			if left == cus {
-				break LABLE
-			}
-			left = cus
-		case nums[cus] == target:
-			for i := cus; i < length && nums[i] == target; i++ {
-				ret++
-			}
-			for i := cus - 1; i >= 0 && nums[i] == target; i-- {
-				ret++
-			}
-			return ret
+	times := 0
+	for _, v := range nums {
+		if v == target {
+			times++
+		} else if v > target {
+			return times
 		}
 	}
-	return 0
+	return times
 }
