0916065d985e779458c5463206982b209c3f00d6
Heyxk <1521048028@qq.com>
2022-07-29 05:55:00 +0000

    剑指 Offer 50. 第一个只出现一次的字符

--- codingInterviews/50/main.go
+++ codingInterviews/50/main.go
@@ -1,21 +1,21 @@
 package main
 
 func firstUniqChar(s string) byte {
-	slength := len(s)
-	blackList := make(map[byte]bool)
-LABLE:
-	for i := 0; i < slength; i++ {
-		if blackList[s[i]] {
-			continue
-		}
-		for j := i + 1; j < slength; j++ {
-			if s[j] == s[i] {
-				blackList[s[i]] = true
-				continue LABLE
-			}
-		}
-		return s[i]
+	sLength := len(s)
+	if sLength == 0 {
+		return ' '
 	}
-	return ' '
-
+	n := 0
+	for i := 0; i < sLength; {
+		if i != n && s[i] == s[n] {
+			n++
+			if n == sLength {
+				return ' '
+			}
+			i = 0
+		} else {
+			i++
+		}
+	}
+	return s[n]
 }
--- codingInterviews/50/main_test.go
+++ codingInterviews/50/main_test.go
@@ -19,7 +19,6 @@ func Test_firstUniqChar(t *testing.T) {
 		{"case5", args{"abcdef"}, 'a'},
 		{"case6", args{"a"}, 'a'},
 		{"case7", args{"aa"}, ' '},
-
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -27,5 +26,31 @@ func Test_firstUniqChar(t *testing.T) {
 				t.Errorf("firstUniqChar() = %v, want %v", got, tt.want)
 			}
 		})
+	}
+}
+
+func Benchmark_firstUniqChar(b *testing.B) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want byte
+	}{
+		// TODO: Add test cases.
+		{"case1", args{"abaccdeff"}, 'b'},
+		{"case2", args{"aaaccddeeffb"}, 'b'},
+		{"case3", args{""}, ' '},
+		{"case4", args{" "}, ' '},
+		{"case5", args{"abcdef"}, 'a'},
+		{"case6", args{"a"}, 'a'},
+		{"case7", args{"aa"}, ' '},
+	}
+
+	for i := 0; i < b.N; i++ {
+		for _, tt := range tests {
+			firstUniqChar(tt.args.s)
+		}
 	}
 }
