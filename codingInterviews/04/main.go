f85146be0ade5b584358f0e825d46e63b70311cc
Heyxk <1521048028@qq.com>
2022-07-29 04:43:37 +0000

    剑指 Offer 04. 二维数组中的查找

--- codingInterviews/04/main.go
+++ codingInterviews/04/main.go
@@ -1,51 +1,14 @@
 package main
 
 func findNumberIn2DArray(matrix [][]int, target int) bool {
-	yright := len(matrix)
-	yleft := 0
-LABLE0:
-	for cus := (yleft + yright) / 2; yleft < yright; cus = (yleft + yright) / 2 {
-		if len(matrix[cus]) == 0 {
-			break LABLE0
-		}
-		switch {
-		case matrix[cus][0] > target: // 游标值大于tatget right左移动
-			if yright == cus {
-				break LABLE0
-			}
-			yright = cus
-
-		case matrix[cus][0] < target:
-			if yleft == cus {
-				break LABLE0
-			}
-			yleft = cus
-		case matrix[cus][0] == target:
-			return true
-		}
-	}
-
-	for i := 0; i < yright; i++ {
-		xleft := 0
-		xright := len(matrix[i])
-	LABLE1:
-		for cus := (xleft + xright) / 2; xleft < xright; cus = (xleft + xright) / 2 {
-			switch {
-			case matrix[i][cus] > target: // 游标值大于tatget right左移动
-				if xright == cus {
-					break LABLE1
-				}
-				xright = cus
-			case matrix[i][cus] < target:
-				if xleft == cus {
-					break LABLE1
-				}
-				xleft = cus
-			case matrix[i][cus] == target:
+	for _, vx := range matrix {
+		for _, vy := range vx {
+			if vy == target {
 				return true
+			} else if vy > target {
+				break
 			}
 		}
-
 	}
 	return false
 
--- codingInterviews/04/main_test.go
+++ codingInterviews/04/main_test.go
@@ -31,3 +31,32 @@ func Test_findNumberIn2DArray(t *testing.T) {
 		})
 	}
 }
+
+func Benchmark_findNumberIn2DArray(b *testing.B) {
+	type args struct {
+		matrix [][]int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{"caes1", args{[][]int{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}}, 5}, true},
+		{"caes2", args{[][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}, 10}, false},
+		{"caes3", args{[][]int{{10}}, 10}, true},
+		{"caes4", args{[][]int{}, 10}, false},
+		{"caes5", args{[][]int{{-1, 3}}, 3}, true},
+		{"caes6", args{[][]int{{-1, 3}, {2, 4}, {6, 9}}, 6}, true},
+		{"caes7", args{[][]int{{-1}, {2}}, 2}, true},
+		{"caes8", args{[][]int{{1, 3, 5}}, 1}, true},
+		{"caes9", args{[][]int{{}}, 1}, false},
+	}
+	for i := 0; i < b.N; i++ {
+		for _, tt := range tests {
+			findNumberIn2DArray(tt.args.matrix, tt.args.target)
+		}
+	}
+
+}
