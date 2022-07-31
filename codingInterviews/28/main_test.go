98c9ed639180bf0439c5af99bd0f1754f230bf43
Heyxk <1521048028@qq.com>
2022-07-31 03:08:35 +0000

    剑指 Offer 28. 对称的二叉树

--- codingInterviews/28/main_test.go
+++ codingInterviews/28/main_test.go
@@ -70,3 +70,50 @@ func Test_recur(t *testing.T) {
 		})
 	}
 }
+
+func Benchmark_isSymmetric(b *testing.B) {
+
+	type args struct {
+		root *TreeNode
+	}
+
+	a := &TreeNode{1, nil, nil}
+	b1 := &TreeNode{2, nil, nil}
+	c := &TreeNode{2, nil, nil}
+	d := &TreeNode{3, nil, nil}
+	e := &TreeNode{4, nil, nil}
+	f := &TreeNode{4, nil, nil}
+	g := &TreeNode{3, nil, nil}
+	a.Left = b1
+	a.Right = c
+	b1.Left = d
+	b1.Right = e
+	c.Left = f
+	c.Right = g
+
+	aa := &TreeNode{1, nil, nil}
+	bb := &TreeNode{2, nil, nil}
+	cc := &TreeNode{2, nil, nil}
+	ff := &TreeNode{3, nil, nil}
+	gg := &TreeNode{3, nil, nil}
+	aa.Left = cc
+	aa.Right = bb
+	bb.Right = ff
+	cc.Right = gg
+
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{"case1", args{a}, true},
+		{"case2", args{aa}, false},
+		{"case3", args{nil}, true},
+	}
+	for i := 0; i < b.N; i++ {
+		for _, tt := range tests {
+			isSymmetric(tt.args.root)
+		}
+	}
+}
