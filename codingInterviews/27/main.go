15be5449c570ad11d62dc5add364830bf5103790
Heyxk <1521048028@qq.com>
2022-07-31 02:47:08 +0000

    剑指 Offer 27. 二叉树的镜像

--- codingInterviews/27/main.go
+++ codingInterviews/27/main.go
@@ -1,5 +1,11 @@
 package main
 
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
 /**
  * Definition for a binary tree node.
  * type TreeNode struct {
@@ -9,27 +15,10 @@ package main
  * }
  */
 func mirrorTree(root *TreeNode) *TreeNode {
-
-	if root == nil {
-		return nil
-	}
-
-	var queue []*TreeNode
-
-	// root.Left, root.Right = root.Right, root.Left
-	queue = append(queue, root)
-
-	for len(queue) > 0 {
-		tmp := queue[0]
-		queue = queue[1:]
-		if tmp.Left != nil {
-			queue = append(queue, tmp.Left)
-		}
-		if tmp.Right != nil {
-			queue = append(queue, tmp.Right)
-		}
-		tmp.Left, tmp.Right = tmp.Right, tmp.Left
+	if root != nil {
+		root.Left, root.Right = root.Right, root.Left
+		mirrorTree(root.Left)
+		mirrorTree(root.Right)
 	}
 	return root
-
 }
--- codingInterviews/27/main_test.go
+++ codingInterviews/27/main_test.go
@@ -68,3 +68,65 @@ func Test_mirrorTree(t *testing.T) {
 		})
 	}
 }
+
+func Benchmark_mirrortree(b *testing.B) {
+	type args struct {
+		root *TreeNode
+	}
+
+	a := &TreeNode{4, nil, nil}
+	b1 := &TreeNode{2, nil, nil}
+	c := &TreeNode{7, nil, nil}
+	d := &TreeNode{1, nil, nil}
+	e := &TreeNode{3, nil, nil}
+	f := &TreeNode{6, nil, nil}
+	g := &TreeNode{9, nil, nil}
+	a.Left = b1
+	a.Right = c
+	b1.Left = d
+	b1.Right = e
+	c.Left = f
+	c.Right = g
+
+	aa := &TreeNode{4, nil, nil}
+	bb := &TreeNode{2, nil, nil}
+	cc := &TreeNode{7, nil, nil}
+	dd := &TreeNode{1, nil, nil}
+	ee := &TreeNode{3, nil, nil}
+	ff := &TreeNode{6, nil, nil}
+	gg := &TreeNode{9, nil, nil}
+	aa.Left = cc
+	aa.Right = bb
+	bb.Left = ee
+	bb.Right = dd
+	cc.Left = gg
+	cc.Right = ff
+
+	x := &TreeNode{2, nil, nil}
+	y := &TreeNode{1, nil, nil}
+	z := &TreeNode{3, nil, nil}
+	x.Left = y
+	x.Right = z
+
+	xx := &TreeNode{2, nil, nil}
+	yy := &TreeNode{1, nil, nil}
+	zz := &TreeNode{3, nil, nil}
+	xx.Left = zz
+	xx.Right = yy
+
+	tests := []struct {
+		name string
+		args args
+		want *TreeNode
+	}{
+		// TODO: Add test cases.
+		{"case1", args{a}, aa},
+		{"case2", args{nil}, nil},
+		{"case3", args{z}, zz},
+	}
+	for i := 0; i < b.N; i++ {
+		for _, tt := range tests {
+			mirrorTree(tt.args.root)
+		}
+	}
+}
