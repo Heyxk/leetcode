59f7f7435ea58f1df0475b2629a78330efb53e19
Heyxk <1521048028@qq.com>
2022-08-03 13:55:22 +0000

    剑指 Offer 46. 把数字翻译成字符串

--- codingInterviews/46/main_test.go
+++ codingInterviews/46/main_test.go
@@ -22,3 +22,22 @@ func Test_translateNum(t *testing.T) {
 		})
 	}
 }
+
+func Benchmark_translateNum(b *testing.B) {
+	type args struct {
+		num int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{"case1", args{12258}, 5},
+	}
+	for i := 0; i < b.N; i++ {
+		for _, tt := range tests {
+			translateNum(tt.args.num)
+		}
+	}
+}
