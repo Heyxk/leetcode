7e348b031b631289030e28a9ea526d2ece4efd4c
Heyxk <1521048028@qq.com>
2022-08-03 06:00:28 +0000

    剑指 Offer 48. 最长不含重复字符的子字符串

--- codingInterviews/48/main.go
+++ codingInterviews/48/main.go
@@ -1,22 +1,16 @@
 package main
 
 func lengthOfLongestSubstring(s string) int {
-	length := len(s)
-	if length < 2 {
-		return length
-	}
 	m := make(map[byte]int)
-	m[s[0]] = 0
-	left, right, max := 0, 0, 0
-	for right = 1; right < length; right++ {
-		if v, ok := m[s[right]]; ok && v >= left {
-			left = v + 1
+	max := 0
+	for left, right, l := 0, 0, len(s); right < l; right++ {
+		if i, ok := m[s[right]]; ok && i >= left {
+			left = i + 1
 		}
 		m[s[right]] = right
-		if max < (right - left + 1) {
-			max = right - left + 1
+		if max < (right-left)+1 {
+			max = (right - left) + 1
 		}
 	}
 	return max
-
 }
--- codingInterviews/48/main_test.go
+++ codingInterviews/48/main_test.go
@@ -29,3 +29,29 @@ func Test_lengthOfLongestSubstring(t *testing.T) {
 		})
 	}
 }
+
+func Benchmark_lengthOfLongestSubstring(b *testing.B) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{"case1", args{"abcabcdeabcdef"}, 6},
+		{"case2", args{"pwwkew"}, 3},
+		{"case3", args{"abcabcdeabcdef"}, 6},
+		{"case4", args{"bbbbbbb"}, 1},
+		{"case5", args{"abcabcbbbbbbb"}, 3},
+		{"case6", args{""}, 0},
+		{"case7", args{"tmmzuxt"}, 5},
+		{"case8", args{" "}, 1},
+	}
+	for i := 0; i < b.N; i++ {
+		for _, tt := range tests {
+			lengthOfLongestSubstring(tt.args.s)
+		}
+	}
+}
