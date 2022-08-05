1c01097e1fdcc1d9e9f0d5cf8729705670a6f028
Heyxk <1521048028@qq.com>
2022-08-05 13:42:30 +0000

    剑指 Offer 25. 合并两个排序的链表

--- codingInterviews/25/main.go
+++ codingInterviews/25/main.go
@@ -1,5 +1,10 @@
 package main
 
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
 /**
  * Definition for singly-linked list.
  * type ListNode struct {
@@ -20,11 +25,11 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 	for l1 != nil && l2 != nil {
 		if l1.Val <= l2.Val {
 			p.Next = l1
-			p = l1
+			p = p.Next
 			l1 = l1.Next
 		} else {
 			p.Next = l2
-			p = l2
+			p = p.Next
 			l2 = l2.Next
 		}
 
@@ -35,5 +40,4 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 		p.Next = l2
 	}
 	return head.Next
-
 }
