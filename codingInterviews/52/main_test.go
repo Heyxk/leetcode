7960b0b574bb72eddc3cb59ec2b5760fff963503
Heyxk <1521048028@qq.com>
2022-08-05 14:15:55 +0000

    剑指 Offer 52. 两个链表的第一个公共节点

--- codingInterviews/52/main_test.go
+++ codingInterviews/52/main_test.go
@@ -46,3 +46,44 @@ func Test_getIntersectionNode(t *testing.T) {
 		})
 	}
 }
+
+func Benchmark_getIntersectionNode(b *testing.B) {
+
+	type args struct {
+		headA *ListNode
+		headB *ListNode
+	}
+
+	a := &ListNode{4, nil}
+	b1 := &ListNode{1, nil}
+	c := &ListNode{8, nil}
+	d := &ListNode{4, nil}
+	e := &ListNode{5, nil}
+
+	a.Next = b1
+	b1.Next = c
+	c.Next = d
+	d.Next = e
+
+	aa := &ListNode{5, nil}
+	bb := &ListNode{0, nil}
+	cc := &ListNode{1, nil}
+	aa.Next = bb
+	bb.Next = cc
+	cc.Next = c
+
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		// TODO: Add test cases.
+		{"case1", args{a, aa}, c},
+		{"case2", args{nil, nil}, nil},
+	}
+	for i := 0; i < b.N; i++ {
+		for _, tt := range tests {
+			getIntersectionNode(tt.args.headA, tt.args.headB)
+		}
+	}
+}
