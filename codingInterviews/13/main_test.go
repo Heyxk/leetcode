e1b1bdaf249abd20f31dcdd526a4ca893891242f
Heyxk <1521048028@qq.com>
2022-08-07 15:05:40 +0000

    面试题13. 机器人的运动范围

--- codingInterviews/13/main_test.go
+++ codingInterviews/13/main_test.go
@@ -29,3 +29,30 @@ func Test_movingCount(t *testing.T) {
 		})
 	}
 }
+
+func Benchmark_movingCount(b *testing.B) {
+	type args struct {
+		m int
+		n int
+		k int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{"case1", args{2, 3, 1}, 3},
+		{"case2", args{3, 1, 0}, 1},
+		{"case3", args{1, 1, 1}, 1},
+		{"case4", args{1, 1, 0}, 1},
+		{"case5", args{1, 1, 2}, 1},
+		{"case6", args{1, 2, 1}, 2},
+	}
+	for i := 0; i < b.N; i++ {
+
+		for _, tt := range tests {
+			movingCount(tt.args.m, tt.args.n, tt.args.k)
+		}
+	}
+}
