20385f85cf131bfdd780ff95a2ff526dedc2afa2
Heyxk <1521048028@qq.com>
2022-08-08 10:32:03 +0000

    剑指 Offer 34. 二叉树中和为某一值的路径

--- codingInterviews/34/main_test.go
+++ codingInterviews/34/main_test.go
@@ -47,7 +47,7 @@ func Test_pathSum(t *testing.T) {
 		// TODO: Add test cases.
 		{"case1", args{a, 22}, [][]int{{5, 4, 11, 2}, {5, 8, 4, 5}}},
 		{"case2", args{aa, 5}, [][]int{}},
-		{"case3", args{&TreeNode{-2, nil, &TreeNode{-3, nil,nil}}, -5}, [][]int{{-2, -3}}},
+		{"case3", args{&TreeNode{-2, nil, &TreeNode{-3, nil, nil}}, -5}, [][]int{{-2, -3}}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -58,20 +58,53 @@ func Test_pathSum(t *testing.T) {
 	}
 }
 
-func Test_dfs(t *testing.T) {
+func Benchmark_pathSum(b *testing.B) {
 	type args struct {
 		root   *TreeNode
 		target int
 	}
+
+	a := &TreeNode{5, nil, nil}
+	b1 := &TreeNode{4, nil, nil}
+	c := &TreeNode{8, nil, nil}
+	d := &TreeNode{11, nil, nil}
+	e := &TreeNode{13, nil, nil}
+	f := &TreeNode{4, nil, nil}
+	g := &TreeNode{7, nil, nil}
+	h := &TreeNode{2, nil, nil}
+	i := &TreeNode{5, nil, nil}
+	j := &TreeNode{1, nil, nil}
+
+	a.Left = b1
+	a.Right = c
+	b1.Left = d
+	c.Right = e
+	c.Right = f
+	d.Left = g
+	d.Right = h
+	f.Left = i
+	f.Right = j
+
+	aa := &TreeNode{1, nil, nil}
+	bb := &TreeNode{2, nil, nil}
+	cc := &TreeNode{3, nil, nil}
+
+	aa.Left = bb
+	aa.Right = cc
+
 	tests := []struct {
 		name string
 		args args
+		want [][]int
 	}{
 		// TODO: Add test cases.
+		{"case1", args{a, 22}, [][]int{{5, 4, 11, 2}, {5, 8, 4, 5}}},
+		{"case2", args{aa, 5}, [][]int{}},
+		{"case3", args{&TreeNode{-2, nil, &TreeNode{-3, nil, nil}}, -5}, [][]int{{-2, -3}}},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			dfs(tt.args.root, tt.args.target)
-		})
+	for i := 0; i < b.N; i++ {
+		for _, tt := range tests {
+			pathSum(tt.args.root, tt.args.target)
+		}
 	}
 }
