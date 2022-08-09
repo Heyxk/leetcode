f0fef0802fa4e03c1cd02644d0231a87c23ac04a
Heyxk <1521048028@qq.com>
2022-08-09 14:46:04 +0000

    剑指 Offer 61. 扑克牌中的顺子

--- codingInterviews/61/main_test.go
+++ codingInterviews/61/main_test.go
@@ -29,3 +29,30 @@ func Test_isStraight(t *testing.T) {
 		})
 	}
 }
+
+func Benchmark_isStraight(b *testing.B) {
+
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{"case1", args{[]int{1, 2, 3, 4, 5}}, true},
+		{"case2", args{[]int{0, 0, 1, 2, 5}}, true},
+		{"case3", args{[]int{0, 0, 1, 3, 5}}, true},
+		{"case4", args{[]int{0, 0, 1, 4, 5}}, true},
+		{"case5", args{[]int{0, 2, 1, 4, 5}}, true},
+		{"case6", args{[]int{0, 0, 1, 1, 5}}, false},
+		{"case7", args{[]int{1, 2, 3, 3, 5}}, false},
+		{"case8", args{[]int{1, 2, 3, 5, 6}}, false},
+	}
+	for i := 0; i < b.N; i++ {
+		for _, tt := range tests {
+			isStraight(tt.args.nums)
+		}
+	}
+}
