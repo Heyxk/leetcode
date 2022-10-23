caa14b2c7852fdd9d8a49045a5670bb09d21a9c1
Heyxk <kevinhsiang@outlook.com>
2022-10-23 14:20:29 +0000

    剑指 Offer 35. 复杂链表的复制

--- codingInterviews/35/main.go
+++ codingInterviews/35/main.go
@@ -19,33 +19,19 @@ func copyRandomList(head *Node) *Node {
 	if head == nil {
 		return nil
 	}
-	input := map[*Node]int{}
-	// output := map[int]*Node{}
-	output := []*Node{}
-
-	retHead := &Node{head.Val, nil, nil}
-	input[head] = 0
-	// output[0] = retHead
-	output = append(output, retHead)
-	for n, inPtr, outPtr := 1, head.Next, retHead; inPtr != nil; n++ {
-		input[inPtr] = n
-		node := &Node{inPtr.Val, nil, nil}
-		// output[n] = node
-		output = append(output, node)
-		outPtr.Next = node
-		outPtr = outPtr.Next
-		inPtr = inPtr.Next
+	m := map[*Node]*Node{}
+	ptr := head
+	for ptr != nil {
+		m[ptr] = &Node{ptr.Val, nil, nil}
+		ptr = ptr.Next
 	}
-	for outPtr := retHead; outPtr != nil; outPtr = outPtr.Next {
-		if head.Random == nil {
-			outPtr.Random = nil
 
-		} else {
-			v := input[head.Random]
-			outPtr.Random = output[v]
-		}
-		head = head.Next
+	ptr = head
 
+	for ptr != nil {
+		m[ptr].Next = m[ptr.Next]
+		m[ptr].Random = m[ptr.Random]
+		ptr = ptr.Next
 	}
-	return retHead
+	return m[head]
 }
