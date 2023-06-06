8546479091a4c9952d32632adf278eac95b4f23c
Heyxk <kevinhsiang@outlook.com>
2023-06-06 11:11:36 +0800

    剑指 Offer 32 - III. 从上到下打印二叉树 III

---
+++ leetgo/go/Offer-32-III/solution.go
@@ -0,0 +1,51 @@
+// Created by k at 2023/06/05 17:54
+// https://leetcode.cn/problems/cong-shang-dao-xia-da-yin-er-cha-shu-iii-lcof/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	. "github.com/j178/leetgo/testutils/go"
+)
+
+// @lc code=begin
+
+func levelOrder(root *TreeNode) (ans [][]int) {
+	if root == nil {
+		return [][]int{}
+	}
+	queue := []*TreeNode{root}
+	for len(queue) != 0 {
+		var tmp []int
+		l := len(queue)
+		for i := 0; i < l; i++ {
+			node := queue[i]
+			if len(ans)%2 != 0 {
+				tmp = append(tmp, node.Val)
+			} else {
+				tmp = append([]int{node.Val}, tmp...)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+		}
+		queue = queue[l:]
+		ans = append(ans, tmp)
+	}
+	return
+}
+
+// @lc code=end
+
+func main() {
+	stdin := bufio.NewReader(os.Stdin)
+	root := Deserialize[*TreeNode](ReadLine(stdin))
+	ans := levelOrder(root)
+	fmt.Println("output: " + Serialize(ans))
+}
