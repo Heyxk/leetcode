0ed359279ec144bce85666f2e63ee3c38eb4edfd
Heyxk <kevinhsiang@outlook.com>
2023-06-06 11:18:18 +0800

    剑指 Offer 58 - I. 翻转单词顺序

--- codingInterviews/58/main.go
+++ codingInterviews/58/main.go
@@ -2,7 +2,7 @@ package main
 
 func reverseLeftWords(s string, n int) string {
 	l := len(s)
-	ret := make([]byte, 0)
+	ret := make([]byte, 0, l)
 	for i := n; i < l; i++ {
 		ret = append(ret, s[i])
 	}
@@ -10,5 +10,20 @@ func reverseLeftWords(s string, n int) string {
 		ret = append(ret, s[i])
 	}
 	return string(ret)
+}
 
+func reverseLeftWords2(s string, n int) string {
+	l := len(s)
+	ret := make([]rune, 0, l)
+	for i, v := range s {
+		if i >= n {
+			ret = append(ret, v)
+		}
+	}
+	for i, v := range s {
+		if i < n {
+			ret = append(ret, v)
+		}
+	}
+	return string(ret)
 }
