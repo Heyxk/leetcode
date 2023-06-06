307742a117cd21fda96eba9dfacc90a902b86720
Heyxk <kevinhsiang@outlook.com>
2023-06-06 11:16:33 +0800

    剑指 Offer 06. 从尾到头打印链表

--- codingInterviews/06/main.go
+++ codingInterviews/06/main.go
@@ -13,11 +13,31 @@ type ListNode struct {
 	Next *ListNode
 }
 
-func reversePrint(head *ListNode) []int {
+func reversePrint1(head *ListNode) []int {
 	ret := []int{}
 	for head != nil {
 		ret = append([]int{head.Val}, ret...)
 		head = head.Next
 	}
+	return ret
+}
+
+var i, j int
+var ret []int
+
+func help(head *ListNode) {
+	if head == nil {
+		ret = make([]int, i)
+		return
+	}
+	i++
+	help(head.Next)
+	ret[j] = head.Val
+	j++
+}
+
+func reversePrint(head *ListNode) []int {
+	i, j = 0, 0
+	help(head)
 	return ret
 }
