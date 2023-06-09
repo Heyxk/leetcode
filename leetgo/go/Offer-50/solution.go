69f19f191233a47faa7ae0ae0860cacc1c6c546a
Heyxk <kevinhsiang@outlook.com>
2023-06-09 14:17:51 +0800

    剑指 Offer 50. 第一个只出现一次的字符

--- leetgo/go/Offer-50/solution.go
+++ leetgo/go/Offer-50/solution.go
@@ -1,4 +1,4 @@
-// Created by k at 2023/06/05 15:45
+// Created by k at 2023/06/09 11:31
 // https://leetcode.cn/problems/di-yi-ge-zhi-chu-xian-yi-ci-de-zi-fu-lcof/
 
 package main
@@ -15,11 +15,11 @@ import (
 
 func firstUniqChar(s string) (ans byte) {
 	m := make(map[rune]int)
-	for _, v := range s {
-		m[v] += 1
+	for _, c := range s {
+		m[c] += 1
 	}
-	for i, v := range s {
-		if m[v] == 1 {
+	for i, c := range s {
+		if m[c] == 1 {
 			return s[i]
 		}
 	}
@@ -32,5 +32,6 @@ func main() {
 	stdin := bufio.NewReader(os.Stdin)
 	s := Deserialize[string](ReadLine(stdin))
 	ans := firstUniqChar(s)
-	fmt.Println("output: " + Serialize(ans))
+
+	fmt.Println("\noutput:", Serialize(ans))
 }
