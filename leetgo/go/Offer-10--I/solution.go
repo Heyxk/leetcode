5c263529109ad9571139a813e703bfa7cb3526a6
Heyxk <kevinhsiang@outlook.com>
2023-06-11 16:05:05 +0800

    剑指 Offer 10- I. 斐波那契数列

--- leetgo/go/Offer-10--I/solution.go
+++ leetgo/go/Offer-10--I/solution.go
@@ -1,13 +1,9 @@
-// Created by k at 2023/06/06 14:03
+// Created by k at 2023/06/11 15:54
 // https://leetcode.cn/problems/fei-bo-na-qi-shu-lie-lcof/
 
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-
 	. "github.com/j178/leetgo/testutils/go"
 )
 
@@ -16,16 +12,9 @@ import (
 func fib(n int) (ans int) {
 	a, b := 0, 1
 	for i := 0; i < n; i++ {
-		a, b = b, (a+b) % 1000000007
+		a, b = b, (a+b)%1000000007
 	}
 	return a
 }
 
 // @lc code=end
-
-func main() {
-	stdin := bufio.NewReader(os.Stdin)
-	n := Deserialize[int](ReadLine(stdin))
-	ans := fib(n)
-	fmt.Println("output: " + Serialize(ans))
-}
