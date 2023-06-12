30d57951e4834d728d1c3b48ddf5462cafc1dc0e
Heyxk <kevinhsiang@outlook.com>
2023-06-12 10:35:03 +0800

    剑指 Offer 42. 连续子数组的最大和

--- leetgo/go/Offer-42/solution.go
+++ leetgo/go/Offer-42/solution.go
@@ -1,39 +1,31 @@
-// Created by k at 2023/06/06 17:11
+// Created by k at 2023/06/12 10:20
 // https://leetcode.cn/problems/lian-xu-zi-shu-zu-de-zui-da-he-lcof/
 
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-
 	. "github.com/j178/leetgo/testutils/go"
 )
 
 // @lc code=begin
 
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
 func maxSubArray(nums []int) (ans int) {
-	max := func(a, b int) int {
-		if a > b {
-			return a
+	ans, maxSum := nums[0], nums[0]
+	for i := 1; i < len(nums); i++ {
+		maxSum = max(nums[i], maxSum+nums[i])
+		if ans < maxSum {
+			ans = maxSum
 		}
-		return b
 	}
-	ret, sum := nums[0], nums[0]
-	for _, v := range nums[1:] {
-		sum = max(v, sum+v)
-		ret = max(sum, ret)
-	}
-	return ret
+
+	return ans
 }
 
 // @lc code=end
-
-func main() {
-	stdin := bufio.NewReader(os.Stdin)
-	nums := Deserialize[[]int](ReadLine(stdin))
-	ans := maxSubArray(nums)
-
-	fmt.Println("\noutput:", Serialize(ans))
-}
