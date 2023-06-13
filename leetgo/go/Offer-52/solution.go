761a8c450e0f370487438f37d0d36357911bcd95
Heyxk <kevinhsiang@outlook.com>
2023-06-13 15:43:41 +0800

    剑指 Offer 52. 两个链表的第一个公共节点

---
+++ leetgo/go/Offer-52/solution.go
@@ -0,0 +1,46 @@
+// Created by k at 2023/06/13 15:38
+// https://leetcode.cn/problems/liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian-lcof/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	. "github.com/j178/leetgo/testutils/go"
+)
+
+// @lc code=begin
+
+func getIntersectionNode(headA, headB *ListNode) (ans *ListNode) {
+	node1, node2 := headA, headB
+	for node1 != node2 {
+		if node1 == nil {
+			node1 = headB
+		} else {
+			node1 = node1.Next
+		}
+		if node2 == nil {
+			node2 = headA
+		} else {
+			node2 = node2.Next
+		}
+	}
+
+	return node1
+}
+
+// @lc code=end
+
+func main() {
+	stdin := bufio.NewReader(os.Stdin)
+	intersectVal := Deserialize[int](ReadLine(stdin))
+	listA := Deserialize[*ListNode](ReadLine(stdin))
+	listB := Deserialize[*ListNode](ReadLine(stdin))
+	skipA := Deserialize[int](ReadLine(stdin))
+	skipB := Deserialize[int](ReadLine(stdin))
+	ans := getIntersectionNode(intersectVal, listA, listB, skipA, skipB)
+
+	fmt.Println("\noutput:", Serialize(ans))
+}
