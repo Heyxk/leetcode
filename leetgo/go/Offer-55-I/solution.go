99a464fb4d04e8a4b98a621aa8f82f6ab7377d95
Heyxk <kevinhsiang@outlook.com>
2023-06-19 15:34:14 +0800

    剑指 Offer 55 - I. 二叉树的深度

---
+++ leetgo/go/Offer-55-I/solution.go
@@ -0,0 +1,48 @@
+// Created by k at 2023/06/19 15:07
+// https://leetcode.cn/problems/er-cha-shu-de-shen-du-lcof/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	. "github.com/j178/leetgo/testutils/go"
+)
+
+// @lc code=begin
+
+func maxDepth(root *TreeNode) (ans int) {
+	if root == nil {
+		return 0
+	}
+	queue := []*TreeNode{root}
+
+	for len(queue) != 0 {
+		ans++
+		n := len(queue)
+		for i := 0; i < n; i++ {
+			node := queue[i]
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+		queue = queue[n:]
+	}
+
+	return
+}
+
+// @lc code=end
+
+func main() {
+	stdin := bufio.NewReader(os.Stdin)
+	root := Deserialize[*TreeNode](ReadLine(stdin))
+	ans := maxDepth(root)
+
+	fmt.Println("\noutput:", Serialize(ans))
+}
