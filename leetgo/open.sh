17bfe0a160cc1129cfe2124081d46dd62d4a31f0
Heyxk <kevinhsiang@outlook.com>
2024-05-11 16:22:55 +0800

    fix: open.sh 替换带空格内容和打开文件问题

--- leetgo/open.sh
+++ leetgo/open.sh
@@ -4,9 +4,9 @@
 #{{.Folder}} {{.CodeFile}} {{.DescriptionFile}}
 
 if [[ "$(echo $2 | grep 'solution.cpp')" != "" ]]; then
-    old_dir=$(jq -r '.[].directory' .vscode/compile_commands.json)
-    sed -i s%$old_dir%$1%g .vscode/compile_commands.json
-    code $2 $3
+    cat .vscode/compile_commands.json | jq ".[].directory |= \"$1\"" > .vscode/_compile_commands.json
+    mv .vscode/_compile_commands.json .vscode/compile_commands.json
+    code "$2" "$3"
 else
-    code $2 $3
+    code "$2" "$3"
 fi
